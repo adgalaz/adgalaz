529eca0a2fe15ee0672bd274fda4ffc464ef9043
adgalaz <adgalaz@gmail.com>
2018-03-18 20:42:23 -0300

    agregando animacion y demos de trabajos

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,11 @@
 $(document).ready(function () {
+//scrollspy
 	$('body').scrollspy({ target: '#navbar' })
+
+//scrollreveal
+window.sr = ScrollReveal({ reset: true, mobile: true });
+
+sr.reveal('.reveal', { duration: 2000 });
 });
 
 
